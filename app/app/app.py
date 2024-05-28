ef3113eb12c68b5db9c0d25f4a43157b38a2aaed
Mohit Nathani <mohit.nathani2@gmail.com>
2024-05-28 23:08:58 +0530

    update model

--- app/app/app.py
+++ app/app/app.py
@@ -1,3 +1,5 @@
+import os
+
 import streamlit as st
 from tensorflow.keras.models import load_model
 import numpy as np
@@ -10,14 +12,34 @@ import keras
 MODEL = tf.keras.models.load_model('models/model.keras')
 CLASS_NAMES = ["Early Blight", "Late Blight", "Normal"]
 st.title("Potato Disease Classification")
-st.logo("app/app/static/logo.png")
-imgPath = st.file_uploader("Upload a image", accept_multiple_files=True)
-
+# st.logo("app/app/static/logo.png")
+col1, col2 = st.columns(2)
+with col1:
+    imgPath = st.file_uploader("Upload a image", accept_multiple_files=True)
+with col2:
+    st.text("")
+    st.text("")
+    st.text("")
+    clearButton = st.button("clear")
 
 placeholder = []
 if imgPath is not None:
     for i in imgPath:
         placeholder.append(st.image(i))
+
+
+if clearButton:
+    st.write("Cleared")
+    for place in placeholder:
+        place.empty()
+else:
+    st.write("")
+    st.write("")
+
+
+testButton = st.button("Test Images")
+
+
 
 
 
@@ -33,8 +55,35 @@ def getPrediction(imgPath):
     predicted_class = np.argmax(predictions)
     return predicted_class, predictions
 
+test_col, sub_col = st.columns(2)
+
+if testButton:
+
+
+    imgPaths = "testing"
+    images = os.listdir(imgPaths)
+    for path in images:
+        currentPath = imgPaths + "/" + path
+        col1, col2 = st.columns(2)
+        potato_class, level = getPrediction(currentPath)
+        level = round(max(level[0]) * 100, 2)
+        with col1:
+            st.image(currentPath)
+        with col2:
+            st.text("")
+            st.text("")
+            st.text("")
+            st.text("")
+            st.text("")
+            st.write(f"Potato is {CLASS_NAMES[potato_class]}")
+            st.write(f"Confidence level: {level}")
+
+
+
+
 
 if st.button("Submit"):
+
     for place in placeholder:
         place.empty()
     if imgPath is not None:
